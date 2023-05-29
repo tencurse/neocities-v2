80083946304e54313a8f0167e9fd87cebe697686
tencurse <tencurse@gmail.com>
2023-05-29 16:58:22 -0700

    Update config to include markdown plugins

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,33 @@
+const eleventyDevServer = "@11ty/eleventy-dev-server";
+const eleventyBrowsersync = "@11ty/eleventy-server-browsersync";
+const markdownIt = require("markdown-it");
+const mdFootnote = require("markdown-it-footnote");
+const mdTaskList = require("markdown-it-task-lists");
+
 module.exports = function (eleventyConfig) {
+  let mdOptions = {
+    html: true,
+    breaks: true,
+    linkify: true,
+  };
+
+  let mdTasklistOptions = {
+    label: true,
+  };
+
+  eleventyConfig.setLibrary("md", markdownIt(mdOptions));
+  eleventyConfig.amendLibrary("md", (mdLib) => mdLib.use(mdFootnote));
+  eleventyConfig.amendLibrary("md", (mdLib) =>
+    mdLib.use(mdTaskList, mdTasklistOptions)
+  );
+
+  eleventyConfig.setServerOptions({
+    module: eleventyBrowsersync,
+    watch: true,
+    files: ["src/*"],
+    logLevel: "debug",
+  });
+
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addPassthroughCopy("./src/fonts");
   eleventyConfig.addPassthroughCopy("./src/js");
