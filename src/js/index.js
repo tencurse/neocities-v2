403d88e1e91e799b78d0238258a4f40d372beae0
tencurse <tencurse@gmail.com>
2023-05-29 16:58:22 -0700

    Move files to src

---
+++ src/js/index.js
@@ -0,0 +1,43 @@
+// 🔥🔥🔥 WELCOME TO MY CONVOLUTED JAVASCRIPT 🔥🔥🔥
+
+document.addEventListener("DOMContentLoaded", function () {
+  const btn = document.querySelector(".toggle-color-scheme");
+  const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+  const currentTheme = localStorage.getItem("theme");
+  let theme = "";
+  const darkMessage = "Turn on the lights 🔆";
+  const lightMessage = "Turn off the lights 🌙";
+
+  if (prefersDarkScheme.matches) {
+    if (currentTheme === null || currentTheme === "dark") {
+      theme = "dark";
+      btn.innerHTML = darkMessage;
+      localStorage.setItem("theme", theme);
+      document.body.classList.toggle("dark-theme");
+    } else {
+      theme = "light";
+      btn.innerHTML = lightMessage;
+      localStorage.setItem("theme", theme);
+      document.body.classList.toggle("light-theme");
+    }
+  } else {
+    theme = "light";
+    btn.innerHTML = lightMessage;
+    localStorage.setItem("theme", theme);
+    document.body.classList.toggle("light-theme");
+  }
+
+  btn.addEventListener("click", function () {
+    document.body.classList.toggle("light-theme");
+    document.body.classList.toggle("dark-theme");
+    if (currentTheme === "dark" || theme === "dark") {
+      theme = "light";
+      btn.innerHTML = lightMessage;
+    } else {
+      theme = "dark";
+      btn.innerHTML = darkMessage;
+    }
+
+    localStorage.setItem("theme", theme);
+  });
+});
