1d53a0f78f3f1698478490b59c4c0caaa0cf93ad
tencurse <tencurse@gmail.com>
2023-05-29 16:58:22 -0700

    Add img folder in passthrough

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addPassthroughCopy("./src/fonts");
   eleventyConfig.addPassthroughCopy("./src/js");
+  eleventyConfig.addPassthroughCopy("./src/img");
+
   return {
     dir: {
       input: "src",
