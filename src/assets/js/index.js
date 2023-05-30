6bd01391f5b4e2ae11f44567e076c6c4f542135b
tencurse <tencurse@gmail.com>
2023-05-30 12:30:03 -0700

    Update theme switcher script

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,43 +1,20 @@
 // 🔥🔥🔥 WELCOME TO MY CONVOLUTED JAVASCRIPT 🔥🔥🔥
 
 document.addEventListener("DOMContentLoaded", function () {
-  const btn = document.querySelector(".toggle-color-scheme");
-  const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
-  const currentTheme = localStorage.getItem("theme");
-  let theme = "";
-  const darkMessage = "Turn on the lights";
-  const lightMessage = "Turn off the lights";
+  const themeSwitcher = document.getElementById("theme-switcher");
+  const currentTheme =
+    localStorage.getItem("theme") ||
+    (window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light");
 
-  if (prefersDarkScheme.matches) {
-    if (currentTheme === null || currentTheme === "dark") {
-      theme = "dark";
-      btn.innerHTML = darkMessage;
-      localStorage.setItem("theme", theme);
-      document.documentElement.classList.toggle("dark-theme");
-    } else {
-      theme = "light";
-      btn.innerHTML = lightMessage;
-      localStorage.setItem("theme", theme);
-      document.documentElement.classList.toggle("light-theme");
-    }
-  } else {
-    theme = "light";
-    btn.innerHTML = lightMessage;
-    localStorage.setItem("theme", theme);
-    document.documentElement.classList.toggle("light-theme");
+  if (currentTheme) {
+    document.documentElement.dataset.theme = currentTheme;
+    themeSwitcher.value = currentTheme;
   }
 
-  btn.addEventListener("click", function () {
-    document.documentElement.classList.toggle("light-theme");
-    document.documentElement.classList.toggle("dark-theme");
-    if (currentTheme === "dark" || theme === "dark") {
-      theme = "light";
-      btn.innerHTML = lightMessage;
-    } else {
-      theme = "dark";
-      btn.innerHTML = darkMessage;
-    }
-
-    localStorage.setItem("theme", theme);
-  });
+  themeSwitcher.onchange = (e) => {
+    document.documentElement.dataset.theme = e.target.value;
+    localStorage.setItem("theme", e.target.value);
+  };
 });
